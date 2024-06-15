4108822725a5c64ba0a1ea0c3a9d95adaa9aa1ce
peroquenariz <el.ace100@gmail.com>
2024-06-15 19:46:17 -0300

    Agregado main de prueba, solo contiene funcionalidad base de menu.

--- main.c
+++ main.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "librerias/mock.h"
+#include "estructuras/usuario.h"
+#include "estructuras/pelicula.h"
+#include "estructuras/fecha.h"
+#include "estructuras/interfaz.h"
 
 int main()
 {
     srand(time(NULL));
+
+//    stMemoria memoria = inicializarMemoria(); // Cargo archivos a memoria
+//    stControlador controlador = inicializarControlador(&memoria);
+
+    // TODO: cargar archivos a memoria
+
+    stInterfaz interfaz = inicializarInterfaz();
+    ejecutarInterfaz(interfaz); // TODO: pasar memoria a interfaz
+
+    // TODO: guardar memoria en archivos
+//    memoria.guardar();
+
     return 0;
 }
